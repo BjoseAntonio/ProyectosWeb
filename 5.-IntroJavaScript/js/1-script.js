fbe1baa51db2bd643ac9e6f2bf77249910b87e16
BJoseAntonio <bas.99jose@gmail.com>
2024-01-25 22:13:19 -0700

    IntroJs

--- 5.-IntroJavaScript/js/1-script.js
+++ 5.-IntroJavaScript/js/1-script.js
@@ -1,3 +1,21 @@
-// alert("Hola mundo")
+alert("Hola mundo")
 
 // Variables 
+var producto = 'Audifonos Gamer'; //inicio de variable con asignacion de valor
+
+var disponible; //Varible sin valor
+
+var producto = true //Reasignando el valor de la varible 
+
+producto = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Electronicos';
+
+//Estilos para las variables 
+var nombre_prodcuto = 'Monitor HD' //Underscore
+var nombreProducto = 'Monitor HD' //Camelcase
+var NombreProducto = 'Monitor HD' //Pascal case
+var nombreproducto = 'Monitor HD' //No suele ser muy utilizado el nombrar una variable con este estilio
+
