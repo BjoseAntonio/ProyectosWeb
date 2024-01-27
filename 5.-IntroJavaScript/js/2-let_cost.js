80a199d8a6f9df6bdedb4665f39e2e586778ffdf
BJoseAntonio <bas.99jose@gmail.com>
2024-01-26 22:08:03 -0700

    IntroJs

---
+++ 5.-IntroJavaScript/js/2-let_cost.js
@@ -0,0 +1,12 @@
+// Variables con let ===> Puede modificarse el valor o crear una variable sin valor 
+
+let compra = true
+
+compra = true
+
+let carrito 
+
+// Variables con const  ===> no se puede modificar su valor o reasignar y siempre la variable debe contar con un valor
+// const compra2  //sin valor la constante es un error 
+const compra2 = true
+const seccion_compra = 'Tecnologia'
