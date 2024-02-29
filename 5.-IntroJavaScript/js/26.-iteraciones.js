60b1f0d5f4e7f18133504ceaffc609be113db5af
BJoseAntonio <bas.99jose@gmail.com>
2024-02-28 21:29:16 -0700

    IntroJS

--- 5.-IntroJavaScript/js/26.-iteraciones.js
+++ 5.-IntroJavaScript/js/26.-iteraciones.js
@@ -8,6 +8,24 @@
 
 //con la iteraciones nos facilita las impreciones o exprarar mas en los datos facilmente y llegar hasta el objetivo que se requiere 
 //For loop
-for(let i = 0; i <= 10; i++){
-    console.log(i)
-}   
+// for(let i = 1; i <= 100; i++){
+//     if( i % 2 === 0){
+//         console.log(`El numero ${i} es par`)
+//     }else{
+//         console.log(`numero ${i} impar`)
+//     }
+// }   
+
+
+const carrito = [
+    {nombre: 'Laptop', precio: 50},
+    {nombre: 'Monitor', precio: 10},
+    {nombre: 'Pc', precio: 20},
+    {nombre: 'Lapiz', precio: 30},
+    {nombre: 'Teclado', precio: 550},
+    {nombre: 'Luz led', precio: 56},
+];
+
+for(let ){
+
+}
