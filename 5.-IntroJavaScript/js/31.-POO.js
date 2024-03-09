5c15d9e8e4975de08358340dc777d82b4abb72fa
BJoseAntonio <bas.99jose@gmail.com>
2024-03-08 22:08:48 -0700

    IntroJs

--- 5.-IntroJavaScript/js/31.-POO.js
+++ 5.-IntroJavaScript/js/31.-POO.js
@@ -1,6 +1,20 @@
 // Clases 
 
 class Producto{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    formatearProducto(){
+        return `El nombre del producto ${this.nombre} es de un precio de ${this.precio}`;
+    }
 
 }
+
+ const producto2 = new Producto('Monitor', 4500);
+ const producto3 = new Producto('Tablet', 6700);
+
+ console.log(producto2);
+ console.log(producto2.formatearProducto())
+ 
