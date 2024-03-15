a9a477bd6da7ca7ab4dedcb6cf61b1651667cb05
BJoseAntonio <bas.99jose@gmail.com>
2024-03-14 22:12:24 -0700

    IntroJS

--- 5.-IntroJavaScript/js/34.-PromisesJS.js
+++ 5.-IntroJavaScript/js/34.-PromisesJS.js
@@ -1,1 +1,10 @@
 //Promises es una promesa que refleja un valor que estara disponible ahora, en un futuro o nunca
+const usuarioAutenticado = new Promise((resolve, reject) => {
+    const auth = true;
+
+    if(auth){
+        resolve('Usuario Autenticado'); //El promise se cumple en esta funcion 
+    }else{
+        reject('No se puede iniciar sesión');
+    }
+});
