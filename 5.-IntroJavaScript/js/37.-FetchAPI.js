aecdd9381a7fc0b9d0a9cc4a8f4d48df8fdbe0c2
BJoseAntonio <bas.99jose@gmail.com>
2024-04-07 22:07:37 -0700

    IntroJS

--- 5.-IntroJavaScript/js/37.-FetchAPI.js
+++ 5.-IntroJavaScript/js/37.-FetchAPI.js
@@ -9,27 +9,32 @@
 
 const archivoEmpleados = 'empleados.json';
 
-function obtenerEmpleados(){
-    fetch(archivoEmpleados)
-        .then(resultado =>{
-            // console.log(resultado);
-            //Status:200 == conexion exitosa 
-            //Status:404 == sin conexion
-            return resultado.json();
-        })
-        .then(datos => {
-            // console.log(datos.empleados);
+ async function obtenerEmpleados(){
+    // fetch(archivoEmpleados)
+    //     .then(resultado =>{
+    //         // console.log(resultado);
+    //         //Status:200 == conexion exitosa 
+    //         //Status:404 == sin conexion
+    //         return resultado.json();
+    //     })
+    //     .then(datos => {
+    //         // console.log(datos.empleados);
 
-            const {empleados} = datos;
+    //         const {empleados} = datos;
 
-            empleados.forEach(empleado => {
-                // console.log(empleado);
-                console.log(empleado.id);
-                console.log(empleado.nombre);
-                console.log(empleado.puesto)
-            });
-        })
-    // fetch('URL DE GOOGLE')
+    //         empleados.forEach(empleado => {
+    //             // console.log(empleado);
+    //             console.log(empleado.id);
+    //             console.log(empleado.nombre);
+    //             console.log(empleado.puesto);
+    //         });
+    //     })
+    // fetch('URL DE GOOGLE') con sulta a un API desde un dominio 
+
+    const resultado = await fetch(archivoEmpleados);
+    const datos = await resultado.json();
+    console.log(datos);
+    
 }
 
 obtenerEmpleados();
