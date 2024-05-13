d88e5732c4b3b749a3f54aa4b3f06210e515d4ae
BJoseAntonio <bas.99jose@gmail.com>
2024-05-12 21:59:16 -0700

    FestivalMusical

--- 7.-FestivalMusica-Sass/gulpfile.js
+++ 7.-FestivalMusica-Sass/gulpfile.js
@@ -6,12 +6,13 @@ const sass = gulpsSass(darkSass)
 
 export function css(done){
     src('src/scss/app.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(dest('build/css'))
 
     done()
 }
 
 export function dev(){
-    watch('src/scss/app.scss', css)
+    // watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css) //Deteción de cambios en todas las carpetas de scss
 }
