a1c86774a2c26426f5af030fec3d5a63213b8a32
BJoseAntonio <bas.99jose@gmail.com>
2024-06-07 23:37:51 -0700

    FestivalMusica

--- 7.-FestivalMusica-Sass/build/js/app.js
+++ 7.-FestivalMusica-Sass/build/js/app.js
@@ -27,20 +27,42 @@ function crearGaleria(){
 }
 
 function mostrarImagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galería'
+    imagen.classList.add('modal-img')
+
     //Generar modal 
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
     modal.onclick = cerrarModal
 
+    //Crear botón cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-modal')
+    cerrarModal.onclick = cerrarModal
+
+    //Agregar y mostrar IMG y BTN
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
     //Agregar al HTML
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
 
-    // console.log(modal)
+    console.log(modal)
 }
 
 function cerrarModal(){
     const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
 
-    modal?.remove() //? sirve como la simplificacíon de un 'if', sí existe un modal que se remueva y si no, no existe ninguna accion.
+    setTimeout(() =>{
+        modal?.remove() //? sirve como la simplificacíon de un 'if', sí existe un modal que se remueva y si no, no existe ninguna accion.
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    },500)
 }
